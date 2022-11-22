e0b8184a0c964be5fe47bbedbad60bfbefe4d786
Carlo Perito <carlop@Carlos-MBP.home>
2022-11-21 19:24:11 -0800

    local storage issue

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './styles/App.css';
 import Header from './components/Header'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import ThemeContextProvider from './contexts/ThemeContext'
 
 function App() {
   return (
     <BrowserRouter>
-      <Header />
+      <ThemeContextProvider>
+        <Header />
+
+      </ThemeContextProvider>
     </BrowserRouter>
 
   )
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,36 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import '../styles/header.css'
 import { Link } from 'react-router-dom'
+import { MdOutlineDarkMode, MdOutlineLightMode } from "react-icons/md";
+import { ThemeContext } from '../contexts/ThemeContext'
+
 
 function Header() {
+    const { darkMode, setDarkMode } = useContext(ThemeContext)
+
+    const handleTheme = () => {
+        setDarkMode(!darkMode)
+        localStorage.setItem('darkMode', !darkMode)
+    }
+
     return (
-        <div className="header-container">
+        <div className={darkMode ? "header-container" : "header-container header-light"}>
             <Link className="logo" to="/">CineTrail</Link>
             <div className="search-container">
                 <input className="search-input" placeholder="Search Movies" />
             </div>
             <div className="header-buttons-container">
-                <button className="theme-button-container">Create and Account</button>
+                <div className="theme-button-container">
+                    {
+                        darkMode ?
+                            <div className="theme-buttons"> <MdOutlineLightMode onClick={handleTheme} className="theme-icon" /><MdOutlineDarkMode className="theme-icon theme-icon-active" /></div>
+                            : <div classNames="theme-buttons"><MdOutlineLightMode className="theme-icon theme-icon-active" /><MdOutlineDarkMode onClick={handleTheme} className="theme-icon" /></div>
+                    }
+                </div>
+                <div>
+                    <button className="create-account-btn">Create an Account</button>
+                </div>
+
             </div>
         </div>
 
