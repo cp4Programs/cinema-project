9a4e2415b4742fe6fa906c9d7b109f5d5794c10c
Carlo Perito <carlop@Carlos-MBP.home>
2022-12-04 10:45:18 -0800

    sliders function complete

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,23 +1,50 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
+import { MdKeyboardArrowRight, MdKeyboardArrowLeft } from 'react-icons/md'
+import '../styles/slider.css'
 
 function Slider({ apiKey, baseUrl }) {
     const [upcomingMovies, setUpcomingMovies] = useState([])
-    const imageBaseUrl =
+    const imageBaseUrl = "https://image.tmdb.org/t/p/w500"
+    const [index, setIndex] = useState(0)
 
 
+    useEffect(() => {
+        axios.get(`${baseUrl}/movie/upcoming?api_key=${apiKey}`)
+            .then(res => {
+                console.log(res.data.results)
+                setUpcomingMovies(res.data.results)
+            })
+            .catch(err => console.log(err))
+    }, [])
 
-        useEffect(() => {
-            axios.get(`${baseUrl}/movie/upcoming?api_key=${apiKey}`)
-                .then(res => {
-                    console.log(res.data.results)
-                    setUpcomingMovies(res.data.results)
-                })
-                .catch(err => console.log(err))
-        }, [])
+    const sliderStyle = {
+        backgroundImage: `url("${imageBaseUrl}${upcomingMovies[index]?.backdrop_path}")`,
+        height: "60vh",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        position: "relative"
+    }
+
+    const handleRight = () => {
+        setIndex(index + 1)
+        if (index === upcomingMovies.length - 1) {
+            setIndex(0)
+        }
+    }
+    const handleLeft = () => {
+        setIndex(index - 1)
+        if (index === 0)
+            setIndex(upcomingMovies.length - 1)
+    }
 
     return (
-        <div>Slider</div>
+        <div style={sliderStyle}>
+            <MdKeyboardArrowLeft onClick={handleLeft} className="left-arrow" />
+            <MdKeyboardArrowRight onClick={handleRight} className="right-arrow" />
+
+        </div>
     )
 }
 
